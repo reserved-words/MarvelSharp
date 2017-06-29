eba506c37d7b49349d9ee891bb36665802d39117
Rhian <rhian.gwawr@gmail.com>
2017-06-29 07:18:01 +0100

    Started implementing StoryParser - small change to test new CI build definition which should merge with GitHub repository if build is successful

--- MarvelSharp/MarvelSharp/Parsers/StoryParser.cs
+++ MarvelSharp/MarvelSharp/Parsers/StoryParser.cs
@@ -11,7 +11,10 @@ namespace MarvelSharp.Parsers
     {
         public override Story Parse(dynamic result)
         {
-            throw new NotImplementedException();
+            return new Story
+            {
+                Id = result.id
+            };
         }
     }
 }
